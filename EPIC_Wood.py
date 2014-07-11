8c04f2faf14cd4baaf7a2f50197f4c791c3907ac
kindleLiu <ft2028739@gmail.com>
2014-07-11 22:53:55 +0800

    upgrade script

--- EPIC_Wood.py
+++ EPIC_Wood.py
@@ -10,23 +10,39 @@ def getWood(device):
 	print 'start single fight'
 	device.touch(640, 360, 'MonkeyDevice.DOWN_AND_UP')
 	time.sleep(5)
+
 	print 'click fight'
 	#start fight
 	device.touch(1200, 600, 'MonkeyDevice.DOWN_AND_UP')
-	time.sleep(11)
+	time.sleep(9)
+
+	print 'click x--step1'
+	#click away the ad
+	device.touch(1200, 50, 'MonkeyDevice.DOWN_AND_UP')
+	time.sleep(2)
+
+	print 'click x--step2'
+	#
+	device.touch(1200, 300, 'MonkeyDevice.DOWN_AND_UP')
+	time.sleep(2)
+
 	print 'click auto fight button'
 	#begin auto fight
 	device.touch(1250, 700, 'MonkeyDevice.DOWN_AND_UP')
 	#if is wood time = 30 else time = 20
 	time.sleep(35)
+
 	print 'click reward'
 	#start get reward
 	device.touch(640, 400, 'MonkeyDevice.DOWN_AND_UP')
 	time.sleep(4)
+
 	print 'click get reward'
 	#start get reward
 	device.touch(780, 700, 'MonkeyDevice.DOWN_AND_UP')
 	time.sleep(14)
+
+	print '*******************'
 	return;
 
 print 'waiting for device connect ...'
